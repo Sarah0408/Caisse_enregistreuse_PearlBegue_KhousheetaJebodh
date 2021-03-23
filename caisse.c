547fa5c0f73736e6b582708cfe8d6c44638daf59
Sarah0408 <khoushee2227@gmail.com>
2021-03-23 15:08:12 +0400

    [caisse]modification

--- caisse.c
+++ caisse.c
@@ -8,13 +8,11 @@ int main()
 	scanf("%lf",&apayer);
 	printf ("Veuillez mettre le montant que vous payez :");
 	scanf("%lf",&payer);
-	if(payer < apayer)
+	while(payer < apayer)
 	{
 	   printf("Please miss out of money :");
    	   scanf("%lf",&apayer);
 	}
-	else
-	{
+	
 	   printf("Merci le montant est exact sur le recu");
-	}
 }	
